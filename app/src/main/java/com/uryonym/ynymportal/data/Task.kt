50f45bb3abe9d3673cf00845d16e09baa3234b9a
yoneyama ryo <uryonym@gmail.com>
2023-04-22 16:31:38 +0900

    Taskモデルを定義

---
+++ app/src/main/java/com/uryonym/ynymportal/data/Task.kt
@@ -0,0 +1,15 @@
+package com.uryonym.ynymportal.data
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Task(
+    val id: String,
+    val title: String,
+    val description: String,
+    @SerialName(value = "deadline_date") val deadlineDate: String,
+    @SerialName(value = "is_complete") val isComplete: Boolean,
+    @SerialName(value = "created_at") val createdAt: String,
+    @SerialName(value = "updated_at") val updatedAt: String,
+)
