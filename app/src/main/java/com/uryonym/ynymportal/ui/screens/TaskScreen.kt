569fe514698e0d0eadd435f65387f79e55b623ff
yoneyama ryo <uryonym@gmail.com>
2023-04-25 22:56:59 +0900

    experimentalMaterilaのoptinをデフォルト設定に変更

--- app/src/main/java/com/uryonym/ynymportal/ui/screens/TaskScreen.kt
+++ app/src/main/java/com/uryonym/ynymportal/ui/screens/TaskScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,7 +35,6 @@ fun TaskList(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskItem(
     task: Task, modifier: Modifier = Modifier
