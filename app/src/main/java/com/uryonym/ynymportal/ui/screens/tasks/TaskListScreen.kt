45bd11d6583280537f3f5a711017228d99294ef8
yoneyama ryo <uryonym@outlook.com>
2023-07-29 07:08:31 +0900

    タスクの未完了と完了でリストの表示を分割するように実装

--- app/src/main/java/com/uryonym/ynymportal/ui/screens/tasks/TaskListScreen.kt
+++ app/src/main/java/com/uryonym/ynymportal/ui/screens/tasks/TaskListScreen.kt
@@ -2,6 +2,10 @@ package com.uryonym.ynymportal.ui.screens.tasks
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -17,6 +21,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,8 +29,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.uryonym.ynymportal.data.Task
@@ -59,7 +66,34 @@ fun TaskListScreen(
         val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
         LazyColumn(modifier = Modifier.padding(padding)) {
-            items(items = uiState.tasks) { task ->
+            items(items = uiState.tasks.filter { !it.isComplete }) { task ->
+                Column {
+                    TaskListItem(
+                        task = task,
+                        onNavigateTaskEdit = onNavigateTaskEdit,
+                        viewModel = viewModel
+                    )
+                    Divider()
+                }
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth()
+                        .height(32.dp)
+                ) {
+                    Text(
+                        text = "Completed",
+                        style = MaterialTheme.typography.titleSmall
+                    )
+                }
+            }
+
+            items(items = uiState.tasks.filter { it.isComplete }) { task ->
                 Column {
                     TaskListItem(
                         task = task,
