e3ab6dc084694cc17558d7dcf464875c961f31f8
yoneyama ryo <uryonym@outlook.com>
2023-08-05 16:58:09 +0900

    不要なimportを削除

--- app/src/main/java/com/uryonym/ynymportal/ui/screens/tasks/TaskListScreen.kt
+++ app/src/main/java/com/uryonym/ynymportal/ui/screens/tasks/TaskListScreen.kt
@@ -1,6 +1,5 @@
 package com.uryonym.ynymportal.ui.screens.tasks
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -28,9 +27,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
