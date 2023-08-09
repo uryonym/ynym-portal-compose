d9d321f39712c7ad19dcb4a005a4faba72340ccb
yoneyama ryo <uryonym@outlook.com>
2023-08-09 22:36:51 +0900

    アプリケーションコンテキスト取得処理を削除

--- app/src/main/java/com/uryonym/ynymportal/YnymPortalApplication.kt
+++ app/src/main/java/com/uryonym/ynymportal/YnymPortalApplication.kt
@@ -1,32 +1,13 @@
 package com.uryonym.ynymportal
 
 import android.app.Application
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
-import java.lang.RuntimeException
 
 @HiltAndroidApp
 class YnymPortalApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        MyContext.onCreateApplication(applicationContext)
     }
-}
 
-class MyContext constructor(val applicationContext: Context) {
-    companion object {
-        private lateinit var instance: MyContext
-
-        fun onCreateApplication(applicationContext: Context) {
-            instance = MyContext(applicationContext)
-        }
-
-        fun getInstance(): MyContext {
-            if (!::instance.isInitialized) {
-                throw RuntimeException("MyContext should be initialized!")
-            }
-            return instance
-        }
-    }
 }
