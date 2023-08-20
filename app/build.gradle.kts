1d3f44fdaa9da7fcf9c2ebb894342387ec2da1e7
yoneyama ryo <uryonym@outlook.com>
2023-08-20 18:48:01 +0900

    Gradleモジュールのバージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,10 +63,10 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha04")
-    implementation("androidx.compose.material:material-icons-extended:1.4.3")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha05")
+    implementation("androidx.compose.material:material-icons-extended:1.5.0")
 
-    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.7.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
