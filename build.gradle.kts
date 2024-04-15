80f83f757df4f50cad52e7d5451bb2bbceb259c8
yoneyama ryo <uryonym@gmail.com>
2024-04-15 22:58:12 +0900

    各モジュールのバージョン更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
-    id("com.google.dagger.hilt.android") version "2.47" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.devToolsKsp) apply false
+    alias(libs.plugins.daggerHiltAndroid) apply false
+    alias(libs.plugins.gmsGoogleServices) apply false
 }
